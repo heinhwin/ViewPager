ffca64f82b4e62b293cdf382b3de799d29f588b8
Hein <heinhwin23@gmail.com>
2021-09-22 17:26:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/viewpagerexample/FragmentAdapter.java
@@ -0,0 +1,34 @@
+package com.example.viewpagerexample;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull  Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch (position)
+        {
+            case 1:
+                return new ViewPager2();
+            case 2:
+                return new ViewPager3();
+            case 3:
+                return new ViewPager4();
+        }
+        return new ViewPager1();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
---
+++ app/src/main/java/com/example/viewpagerexample/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.viewpagerexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
+    FragmentAdapter fragmentAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentAdapter = new FragmentAdapter(fragmentManager, getLifecycle());
+        viewPager.setAdapter(fragmentAdapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Drinks"));
+        tabLayout.addTab(tabLayout.newTab().setText("Cake"));
+        tabLayout.addTab(tabLayout.newTab().setText("Small Plates"));
+        tabLayout.addTab(tabLayout.newTab().setText("Speciality"));
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+    }
+}
